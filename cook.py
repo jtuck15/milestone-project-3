165e563a08d4b499eec7304ff4f31d1891532829
Jimmy Tuck <jim.tuck15@gmail.com>
2019-04-15 23:42:03 +0000

    updated meals page with pictures and for loop

--- cook.py
+++ cook.py
@@ -5,8 +5,22 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+# connecting to the database
+
+app.config['MONGO_DBNAME'] ='cooking_recipes'
+app.config['MONGO_URI'] = 'mongodb+srv://jtuck15:jt511526@jimmyt-2rj89.mongodb.net/cooking_recipes?retryWrites=true'
+
+mongo = PyMongo(app)
+
+# meals rendered from database to the home page
+
+@app.route('/')
+@app.route('/get_meals')
+def get_meals():
+    return render_template('meals.html', course=mongo.db.course.find())
+
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP', '0.0.0.0'), 
-            port=int(os.environ.get('PORT', '5000')), 
+    app.run(host=os.environ.get('IP'), 
+            port=int(os.environ.get('PORT')), 
             debug=True)
