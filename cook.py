744f1cf3e10fe8679c7a010534d6edbd61564762
Jimmy Tuck <jim.tuck15@gmail.com>
2019-04-25 22:33:39 +0000

    fixed edit functionality within the project so you can now directly edit a meal when you click on the edit meal button

--- cook.py
+++ cook.py
@@ -22,7 +22,8 @@ def get_meals():
                             starters=mongo.db.starters.find(), 
                             main_courses=mongo.db.main_courses.find(),
                             desserts=mongo.db.desserts.find(),
-                            side_dishes=mongo.db.side_dishes.find())
+                            side_dishes=mongo.db.side_dishes.find(),
+                            all_recipes = mongo.db.all_recipes.find())
     
 
 @app.route('/add_meal')
@@ -32,30 +33,43 @@ def add_meal():
                             starters = mongo.db.starters.find(),
                             main_courses = mongo.db.main_courses.find(),
                             desserts = mongo.db.desserts.find(),
-                            side_dishes = mongo.db.side_dishes.find())
+                            side_dishes = mongo.db.side_dishes.find(),
+                            all_recipes = mongo.db.all_recipes.find())
                             
 
 @app.route('/insert_meal', methods=['POST'])
 def insert_meal():
-    starters = mongo.db.starters
-    main_courses = mongo.db.main_courses
-    desserts = mongo.db.desserts
-    side_dishes = mongo.db.side_dishes
+    all_recipes = mongo.db.all_recipes
     
-    if request.form.get('course_name') == 'Starters':
-        starters.insert_one(request.form.to_dict())
-        
-    elif request.form.get('course_name') == 'Main Courses':
-        main_courses.insert_one(request.form.to_dict())
-        
-    elif request.form.get('course_name') == 'Desserts':
-        desserts.insert_one(request.form.to_dict())
-        
-    elif request.form.get('course_name') == 'Side Dishes':
-        side_dishes.insert_one(request.form.to_dict())
+    all_recipes.insert_one(request.form.to_dict())
     
     return redirect(url_for('get_meals'))
     
+
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    _recipe = mongo.db.all_recipes.find_one({'_id': ObjectId(recipe_id)})
+    _course = mongo.db.course.find()
+    course_list = [course for course in _course]
+    return render_template('edit_recipe.html', recipe= _recipe, course= course_list)
+    
+
+@app.route('/update_recipe/<recipe_id>', methods=['POST'])
+def update_recipe(recipe_id):
+    all_recipes = mongo.db.all_recipes
+    all_recipes.update( {'_id': ObjectId(recipe_id)},
+    {
+        'meal_name': request.form.get('meal_name'),
+        'course_name': request.form.get('course_name'),
+        'description': request.form.get('description'),
+        'cooking_time': request.form.get('cooking_time'),
+        'author': request.form.get('author'),
+        'ingredients': request.form.get('ingredients'),
+        'method': request.form.get('method'),
+        'picture': request.form.get('picture')
+    })
+    
+    return redirect(url_for('get_meals'))
 
 
 if __name__ == '__main__':
