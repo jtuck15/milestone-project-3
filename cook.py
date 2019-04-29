28e4231f504bc9d4a88d0fa68bf95e8f8c0a9726
Jimmy Tuck <jim.tuck15@gmail.com>
2019-04-29 21:05:02 +0000

    Added styling to many of the pages available on the website

--- cook.py
+++ cook.py
@@ -70,6 +70,39 @@ def update_recipe(recipe_id):
     })
     
     return redirect(url_for('get_meals'))
+    
+
+@app.route('/show_recipe/<recipe_id>')
+def show_recipe(recipe_id):
+    return render_template('recipe.html',
+    recipe=mongo.db.all_recipes.find_one({'_id': ObjectId(recipe_id)}))
+
+
+@app.route('/delete_recipe/<recipe_id>')
+def delete_recipe(recipe_id):
+    mongo.db.all_recipes.remove({'_id': ObjectId(recipe_id)})
+    return redirect(url_for('get_meals'))
+    
+    
+@app.route('/get_starters')
+def get_starters():
+    return render_template('starters.html', all_recipes = mongo.db.all_recipes.find())
+
+
+@app.route('/get_mains')
+def get_mains():
+    return render_template('maincourse.html', all_recipes = mongo.db.all_recipes.find())
+
+@app.route('/get_desserts')
+def get_desserts():
+    return render_template('dessert.html', all_recipes = mongo.db.all_recipes.find())
+
+
+@app.route('/get_sides')
+def get_sides():
+    return render_template('sides.html', all_recipes = mongo.db.all_recipes.find())
+    
+    
 
 
 if __name__ == '__main__':
